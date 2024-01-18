ba8016bdc164473e0d147188f76a2440f98c04c1
iskariott <blablaname4@gmail.com>
2024-01-18 14:01:04 +0200

    update

--- config.js
+++ config.js
@@ -1,16 +1,24 @@
-// 0-99
-const ACCOUNTS_AMOUNT = 100;
-
-const DELAY = [3, 4];
-
 /*
-0 - Сортувати на 4 тижня
-1 - Сортувати на місяць
-2 - NRAND (кількість) рандомних акаунтів
+0 - Сортує ACCOUNTS_AMOUNT на 4 тижня поточного місяця ( 4 транзи в місяць на акк). 
+1 - Сортує ACCOUNTS_AMOUNT на 30 днів (1 транза в місяць на акк)
+2 - Бере NRAND рандомних акаунтів з wallets.txt і проганяє раз в день
 */
 const SORT_TYPE = 2;
 
+/*
+Потрібно тільки якщо вибрано SORT_TYPE 0/1
+*/
+const ACCOUNTS_AMOUNT = 100;
+
+/*
+Потрібно тільки якщо вибрано SORT_TYPE 2
+*/
 const NRAND = 3;
+
+/*
+Затримка між акаунтами
+*/
+const DELAY = [3, 4];
 
 module.exports = {
   ACCOUNTS_AMOUNT,
